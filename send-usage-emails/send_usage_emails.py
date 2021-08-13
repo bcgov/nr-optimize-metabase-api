981aba6e87cea4834d4a917281002047f2a5bc91
Peter Platten <peter.platten@gov.bc.ca>
2021-08-13 15:33:34 -0700

    added in graphing with pyplot

--- send-usage-emails/send_usage_emails.py
+++ send-usage-emails/send_usage_emails.py
@@ -24,20 +24,21 @@ import smtplib
 import psycopg2
 import calendar
 # import glob
-# import time
-# import os
-# import matplotlib.pyplot as plt
+import time
+import os
+import matplotlib.pyplot as plt
 
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
-# from email.mime.image import MIMEImage
+from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 
-# q = pyad.adquery.ADQuery()
-# q.default_ldap_server = "ldaps://plywood.idir.bcgov:636"
 
 """
-# update a user dictionary with AD information
+q = pyad.adquery.ADQuery()
+q.default_ldap_server = "ldaps://plywood.idir.bcgov:636"
+
+# Update a user dictionary with AD information
 def get_ad_info(user):
     idir = user["idir"]
     q.execute_query(["samaccountname", "givenName", "mail"], where_clause=f"samaccountname='{idir}'")
@@ -66,122 +67,8 @@ def get_ad_info(user):
 """
 
 
-# send email to user with previously generated graph
-def send_email(
-    name,
-    prev_month,
-    current_month,
-    dest_directory,
-    recipient,
-    prev_month_data,
-    curr_month_data,
-    prev_month_cost,
-    curr_month_cost,
-):
-    sender = "IITD.Optimize@gov.bc.ca"
-    # recipient = "IITD.Optimize@gov.bc.ca"
-    # cc = "IITD.Optimize@gov.bc.ca"
-
-    # Create message container - the correct MIME type is multipart/alternative.
-    msg = MIMEMultipart("related")
-    msg["Subject"] = "Your H: Drive Usage Report for " + str(current_month)
-    msg["From"] = sender
-    # msg['Cc'] = cc
-    msg["To"] = recipient
-
-    # rcpt = cc.split(',') + [recipient]
-
-    # Create the body of the message (a plain-text and an HTML version).
-    html = (
-        """\
-    <html>
-        <head></head>
-        <body>
-        <p>
-            Hi """
-        + str(name)
-        + """!<br>
-            <br>
-            The Optimization Team is making personalized H: Drive Usage Reports available to NRM users by email on a monthly basis.<br>
-            <br>
-            H: Drive usage information is provided mid-month from the OCIO.
-            Below, you will find a graph highlighting your H: Drive usage for """
-        + str(prev_month)
-        + """ and """
-        + str(current_month)
-        + """.
-            At the time the data usage snapshot was taken, your H: Drive size was """
-        + str(curr_month_data)
-        + """GB,
-            costing your Ministry $"""
-        + str(curr_month_cost)
-        + """ for the month of """
-        + str(current_month)
-        + """.
-            In """
-        + str(prev_month)
-        + """, you used """
-        + str(prev_month_data)
-        + """GB at a cost of $"""
-        + str(prev_month_cost)
-        + """.<br>
-            <br>
-            <img src="cid:image1" alt="Graph" style="width:250px;height:50px;"><br>
-            <br>
-            <b>Why is My Data Usage Important?</b><br>
-            Data storage on the H: Drive is expensive and billed at $2.70 per GB, per month.
-            This communication is meant to raise awareness and encourage you to proactively keep costs down.<br>
-            <br>
-            <b>Did the size of your H:Drive go up this month?</b><br>
-            Here are 3 simple actions to help you reduce your storage expense "footprint":
-            <ol>
-                <li>Delete duplicate files and old drafts (time suggested: 5-10 mins)</li>
-                <li><a href="https://intranet.gov.bc.ca/iit/products-services/technical-support/storage-tips-and-info#Emptyyourrecycling">Empty</a>
-                your Recycle Bin (time suggested: 1 min)</li>
-                <li><a href="https://intranet.gov.bc.ca/iit/onedrive/onedriveinfo?">Move</a> your files to OneDrive (time suggested: 20 mins)</li>
-            </ol>
-            <br>
-            More suggestions on how to reduce can be found on our
-            <a href="https://intranet.gov.bc.ca/iit/products-services/technical-support/storage-tips-and-info">StorageTips and Information page</a>.<br>
-            <br>
-            We welcome your questions, comments, and ideas! Connect with us at IITD.Optimize@gov.bc.ca.<br>
-            <br>
-            Signed,<br>
-            Your Friendly Neighbourhood Optimization Team<br>
-            (Chris, Hannah, Heather, Joseph, Kristal, Lolanda, and Peter)<br>
-            <br>
-            <br>
-            </p>
-            <p style="font-size: 10px">If you do not wish to receive these emails, please reply with the subject line "unsubscribe".</p>
-        </body>
-    </html>
-    """
-    )
-    # Record the MIME types of both parts - text/plain and text/html.
-    body = MIMEText(html, "html")
-    # Attach parts into message container.
-    # According to RFC 2046, the last part of a multipart message, in this case
-    # the HTML message, is best and preferred.
-    msg.attach(body)
-    """
-    # open image and read as binary
-    fp = open(os.path.join(dest_directory, "graph.png"), "rb")
-    msgImage = MIMEImage(fp.read())
-    fp.close()
-
-    # define the image's ID as referenced above
-    msgImage.add_header("Content-ID", "<image1>")
-    msg.attach(msgImage)
-    """
-    # send the message via local SMTP server.
-    s = smtplib.SMTP(constants.SMTP_SERVER)
-    # sendmail function takes 3 arguments: sender's address, recipient's address
-    # and message to send - here it is sent as one string.
-    # s.sendmail(sender, recipient, msg.as_string())
-    s.quit()
-
-
-def get_sample(gb, sample_datetime: datetime):
+# Get a simple formatted "sample" object
+def get_sample(gb: float, sample_datetime: datetime):
     return {
         "gb": gb,
         "sample_datetime": sample_datetime,
@@ -189,6 +76,7 @@ def get_sample(gb, sample_datetime: datetime):
     }
 
 
+# Send an email to the admin with error message
 def send_error_email(error_message):
     msg = MIMEMultipart("related")
     msg["Subject"] = "Script failure"
@@ -207,6 +95,7 @@ def send_error_email(error_message):
     s.quit()
 
 
+# Query metabase db for h drive table, return data dictionary by idir
 def get_hdrive_data():
     conn = None
     data = None
@@ -278,6 +167,7 @@ def get_hdrive_data():
     return data
 
 
+# Calculate and Format $ cost by GB
 def get_gb_cost(gb):
     cost = round(2(gb - 1.5) * 2.7, 2)
     if cost > 0:
@@ -285,8 +175,59 @@ def get_gb_cost(gb):
     return 0
 
 
-def send_idir_email(idir, data):
-    idir_info = data[idir]
+# Generate an graph image's bytes using idir info
+def get_graph_bytes(idir_info):
+    samples = idir_info["samples"]
+    idir = idir_info["name"]
+    # Select plot theme
+    plt.style.use("seaborn-whitegrid")
+    fig = plt.figure()
+    ax1 = plt.axes()
+
+    dates = []
+    gb = []
+    label_names = []
+    for sample in samples:
+        dates.append(sample["sample_datetime"])
+        gb.append(sample["gb"])
+        label_names.append(sample["month"])
+    # Build bar chart with a colour array
+    x = dates
+    y = gb
+    plt.bar(x, y, color=["#e3a82b", "#234075"], alpha=0.9)
+
+    # Apply labels, legends and alignments
+    plt.title(f"{idir} - H: Drive Data Usage", fontsize=14)
+    plt.ylabel("Data size (GB)", fontsize=10)
+    x_axis = ax1.axes.get_xaxis()
+    x_axis.set_visible(False)
+
+    plt.legend(
+        title="Month",
+        fontsize="small",
+        fancybox=True,
+        framealpha=1,
+        shadow=True,
+        bbox_to_anchor=(1.01, 1),
+        labels=label_names,
+        borderaxespad=0
+    )
+    caption = " "
+    fig.text(0.5, 0.01, caption, ha="center")
+    plt.tight_layout()
+
+    # Save the plot to file
+    plt.savefig(os.path.join(os.getcwd(), 'graph.png'))
+    # open image and read as binary
+    fp = open(os.path.join(os.getcwd(), "graph.png"), "rb")
+    image_bytes = fp.read()
+    fp.close()
+
+    return image_bytes
+
+
+# Send an email to the user containing usage information
+def send_idir_email(idir_info):
     samples = idir_info["samples"]
     name = idir_info["name"]
     recipient = idir_info["email"]
@@ -327,7 +268,6 @@ def send_idir_email(idir, data):
         ${month_before_last_cost}.
         """
     html_img = """<br><br><img src="cid:image1" alt="Graph" style="width:250px;height:50px;">"""
-    html_img = html_img
     html_why_important = """
     <br><br>
     <b>Why is My Data Usage Important?</b><br>
@@ -360,7 +300,11 @@ def send_idir_email(idir, data):
     </body>
     </html>
     """
-    html = (html_intro + html_snapshot_taken + html_why_important + html_footer)
+    msgImage = MIMEImage(get_graph_bytes(idir_info))
+    # define the image's ID as referenced above
+    msgImage.add_header("Content-ID", "<image1>")
+    msg.attach(msgImage)
+    html = (html_intro + html_snapshot_taken + html_img + html_why_important + html_footer)
     msg.attach(MIMEText(html, "html"))
     s = smtplib.SMTP(constants.SMTP_SERVER)
     s.sendmail(msg["From"], recipient, msg.as_string())
@@ -369,9 +313,16 @@ def send_idir_email(idir, data):
 
 
 def main(argv):
+    # get a dictionary of { idir : {
+    #       name, email, idir, samples : [{
+    #           gb, sample_datetime, month
+    #       }]
+    #   }
+    # }
     data = get_hdrive_data()
     if data is None:
         return
+
     for idir in data:
         # print the samples
         for sample in data[idir]["samples"]:
@@ -379,81 +330,9 @@ def main(argv):
             sample_datetime = sample["sample_datetime"]
             month = sample["month"]
             print(f"GB: {gb}, Datetime: {sample_datetime}, Month: {month}")
-        send_idir_email(idir, data)
-    # users = {}
-    # get a dictionary of user > usage: {date/data usage}, name, from H Drive Usage
-
-    # remove excluded idirs from dict_of_users
-    # dict_of_users = [user for user in dict_of_users if user["idir"] not in excluded_idirs]
-
-    # explicitly define users for testing so as to not spam all users in the usage report list
-    """
-    if constants.USE_DEBUG_IDIR.upper() == 'TRUE':
-        dict_of_users = [user for user in dict_of_users if user["idir"] == constants.DEBUG_IDIR]
-
-        # get current month and subtract 1 month from it to get previous month and set current month to be equal to it.
-        # This ensures that if it is sent on the first of the new month that the email states the correct months for the data
-        cur_month_number = datetime.now().month
-        cur_month_number = cur_month_number - 1
-        if cur_month_number == 0:
-            cur_month_number = 12
-        prev_month_number = cur_month_number - 1
-        if prev_month_number == 0:
-            prev_month_number = 12
-        # convert current and previous month numbers to text
-        current_month = calendar.month_name[cur_month_number]
-        prev_month = calendar.month_name[prev_month_number]
-
-    for user in dict_of_users:
-        user = get_ad_info(user)
-        if user["all_ad_attributes_found"]:
-            idir = user["idir"]
-            name = user["givenName"]
-            recipient = user["mail"]
-            print(idir + ", " + name + ", " + recipient)
-
-            generate_graph(idir, dest_directory, prev_month, current_month)
-
-            # get_usage(idir,users)
-            df_new = users[users["idir"] == idir]
-            print(len(df_new))
-            print(df_new)
-            if len(df_new) == 2:
-                prev_month_data = df_new["datausage"].iloc[0]
-                prev_month_cost = prev_month_data * 2.7
-                curr_month_data = df_new["datausage"].iloc[1]
-                curr_month_cost = curr_month_data * 2.7
-                if prev_month_data < 1.5:
-                    prev_month_cost = 0
-                if curr_month_data < 1.5:
-                    curr_month_cost = 0
-                prev_month_cost = round(prev_month_cost, 2)
-                curr_month_cost = round(curr_month_cost, 2)
-
-            if len(df_new) == 1:
-                curr_month_data = df_new["datausage"].iloc[0]
-                curr_month_cost = curr_month_data * 2.7
-                prev_month_data = 0
-                prev_month_cost = 0
-                if curr_month_data < 1.5:
-                    curr_month_cost = 0
-                curr_month_cost = round(curr_month_cost, 2)
-
-            send_email(
-                name,
-                prev_month,
-                current_month,
-                dest_directory,
-                recipient,
-                prev_month_data,
-                curr_month_data,
-                prev_month_cost,
-                curr_month_cost,
-            )
-        else:
-            time.sleep(2)
-
-    """
+        # send email to user
+        send_idir_email(data[idir])
+        time.sleep(2)
 
 
 if __name__ == "__main__":
