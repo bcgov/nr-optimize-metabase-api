74f9f0eba639ae99265f888a92dbfcd81575c3d9
ccocker <chris.cocker@gov.bc.ca>
2022-04-14 16:36:14 -0700

    Adding script/updates to update quarterly reporting dashboard using metabase API

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,7 +1,7 @@
 import os
 import dotenv
 
-envPath = os.path.join(os.path.dirname(__file__), '..', '.env')
+envPath = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(envPath):
     print("loading dot env...")
     dotenv.load_dotenv()
@@ -9,3 +9,7 @@ if os.path.exists(envPath):
 BUCKET_NAME = os.environ['BUCKET_NAME']
 BUCKET_USER = os.environ['BUCKET_USER']
 BUCKET_SECRET = os.environ['BUCKET_SECRET']
+
+# Environment Variables for update_quarterly_reporting.py - government email (username) & IDIR Password
+USER_NAME = os.environ['USER_NAME']
+PASSWORD = os.environ['PASSWORD']
