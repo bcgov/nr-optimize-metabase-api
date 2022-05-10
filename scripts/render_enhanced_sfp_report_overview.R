01ce7df10a11e97ede0fbaf0ff39b7cd9844f11a
hhay1981 <58308942+hhay1981@users.noreply.github.com>
2022-05-10 13:25:15 -0700

    minor updates and file renaming

---
+++ scripts/render_enhanced_sfp_report_overview.R
@@ -0,0 +1,21 @@
+# setup
+library(here)
+library(rmarkdown)
+
+# function to render parameters and save html output to file
+render_report = function(data, ministry, quarter, fiscal, collected) {
+  rmarkdown::render(
+    here("scripts", "enhanced_sfp_report_overview.Rmd"), params = list(
+      data = data,
+      ministry = ministry,
+      quarter = quarter,
+      fiscal = fiscal,
+      collected = collected
+    ),
+    output_file = paste0("SFP_Enhanced_Report_", ministry, "_", quarter, "_", fiscal, ".html"),
+    output_dir = here("output"),
+  )
+}
+
+#render, stating parameters
+render_report("2022-01-01_EMLI_Enhanced_Data.csv", "EMLI", "Q4", "FY21-22", "2021-12-22")
