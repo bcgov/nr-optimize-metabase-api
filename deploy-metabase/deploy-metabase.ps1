be5a840a053d0d2a559d02cb7f1ba30f920bb9f3
Peter Platten <peter.platten@gov.bc.ca>
2022-07-26 14:28:56 -0700

    tidying up

--- deploy-metabase/deploy-metabase.ps1
+++ deploy-metabase/deploy-metabase.ps1
@@ -1,5 +1,6 @@
 # Script assumes user is signed in with OC
 
+# Set configuration Values
 $BASE_URL="https://raw.githubusercontent.com/bcgov/nr-showcase-devops-tools/master/tools/metabase/openshift"
 $ADMIN_EMAIL="iitd.optimize@gov.bc.ca"
 $TOOLS_NAMESPACE="15be76-tools"
@@ -8,6 +9,7 @@ $TARGET_NAMESPACE="15be76-prod"
 $PREFIX="nridsco"
 $METABASE_VERSION="v0.41.5"
 
+# Get the database pod names from the origin namespace 
 $ORIGIN_METABASE_DB_POD=oc get pods -n $ORIGIN_NAMESPACE --selector name=metabase-postgresql --field-selector status.phase=Running -o custom-columns=POD:.metadata.name --no-headers
 $ORIGIN_POSTGRES_DB_POD=oc get pods -n $ORIGIN_NAMESPACE --selector name=postgresql --field-selector status.phase=Running -o custom-columns=POD:.metadata.name --no-headers
 
@@ -15,12 +17,8 @@ $ORIGIN_POSTGRES_DB_POD=oc get pods -n $ORIGIN_NAMESPACE --selector name=postgre
 # Can check success using: oc describe rolebinding.rbc -n $TOOLS_NAMESPACE
 oc policy add-role-to-user system:image-puller system:serviceaccount:$TARGET_NAMESPACE`:default -n $TOOLS_NAMESPACE
 
-# Apply quickstart network security policies, which allows:
-# - The namespace to talk to the internet and itself
-# - The pods to talk to the k8s (a.k.a Kubernetes) api
-# Note: The only difference between quickstart-nsp.yaml and the online version is we use NetworkPolicy instead of NetworkSecurityPolicy.
-#       I believe this is a version issue
-#       Original: https://raw.githubusercontent.com/BCDevOps/platform-services/master/security/aporeto/docs/sample/quickstart-nsp.yaml
+# Apply quickstart network security policies
+# Note: This is a different flavor of the original metabase nsps, but works. 
 oc -n $TARGET_NAMESPACE process -f quickstart-nsp.yaml NAMESPACE=$TARGET_NAMESPACE | oc -n $TARGET_NAMESPACE apply -f -
 
 # Deploy the build config to the tools namespace and build :latest image:
@@ -35,7 +33,7 @@ oc exec $ORIGIN_POSTGRES_DB_POD -n $ORIGIN_NAMESPACE -- pg_dumpall -f /tmp/all_p
 New-Item -ItemType Directory -Force -Path temp-metabase
 New-Item -ItemType Directory -Force -Path temp-postgresql
 
-# Copy origin database backups to local temp folder
+# Copy origin database backups to local temp folders
 oc rsync --progress -n $ORIGIN_NAMESPACE $ORIGIN_METABASE_DB_POD`:/tmp/all_metabase_postgresql_dbs.sql ./temp-metabase/
 oc rsync --progress -n $ORIGIN_NAMESPACE $ORIGIN_POSTGRES_DB_POD`:/tmp/all_postgresql_dbs.sql ./temp-postgresql/
 
@@ -67,14 +65,11 @@ $POSTGRES_PASS = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($en
 $encoded_value = oc get secret postgresql -n $ORIGIN_NAMESPACE -o jsonpath="{.data['database-name']}"
 $POSTGRES_DB_NAME = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($encoded_value))
 
-# Create new database deployments
+# Create new database deployments and services
 oc new-app -n $TARGET_NAMESPACE -e POSTGRESQL_USER=$METABASE_DB_USER -e POSTGRESQL_PASSWORD=$METABASE_DB_PASS -e POSTGRESQL_DATABASE=$METABASE_DB_NAME postgresql:10-el8 --name=metabase-postgresql
 oc new-app -n $TARGET_NAMESPACE -e POSTGRESQL_USER=$POSTGRES_USER -e POSTGRESQL_PASSWORD=$POSTGRES_PASS -e POSTGRESQL_DATABASE=$POSTGRES_DB_NAME postgresql:10-el8 --name=postgresql
 
-# Get the new pod names
-$TARGET_METABASE_DB_POD=oc get pods -n $TARGET_NAMESPACE --selector deployment=metabase-postgresql --field-selector status.phase=Running -o custom-columns=POD:.metadata.name --no-headers
-$TARGET_POSTGRES_DB_POD=oc get pods -n $TARGET_NAMESPACE --selector deployment=postgresql --field-selector status.phase=Running -o custom-columns=POD:.metadata.name --no-headers
-
+# Get the new pod names, will need to wait for the postgres pod to finish being created
 WHILE (($null -eq $TARGET_METABASE_DB_POD) -or ($null -eq $TARGET_POSTGRES_DB_POD)) {
     $TARGET_METABASE_DB_POD=oc get pods -n $TARGET_NAMESPACE --selector deployment=metabase-postgresql --field-selector status.phase=Running -o custom-columns=POD:.metadata.name --no-headers
     $TARGET_POSTGRES_DB_POD=oc get pods -n $TARGET_NAMESPACE --selector deployment=postgresql --field-selector status.phase=Running -o custom-columns=POD:.metadata.name --no-headers
@@ -90,9 +85,9 @@ oc rsync --progress --no-perms -n $TARGET_NAMESPACE ./temp-postgresql/ $TARGET_P
 oc exec $TARGET_METABASE_DB_POD -n $TARGET_NAMESPACE -- psql -f /tmp/all_metabase_postgresql_dbs.sql
 oc exec $TARGET_POSTGRES_DB_POD -n $TARGET_NAMESPACE -- psql -f /tmp/all_postgresql_dbs.sql
 
-# Deploy DeployConfig to target namespace
+# Deploy DeployConfig, persistent volume claim, metabase-secret secret, metabase service and route to target namespace
 oc process -n $TOOLS_NAMESPACE -f ./metabase.dc.yaml -p NAMESPACE=$TOOLS_NAMESPACE -p PREFIX=$PREFIX -p VERSION=$METABASE_VERSION -o yaml | oc apply -n $TARGET_NAMESPACE -f -
 
-# Clean up the now migrated database files
+# Clean up the now migrated database files from local temp folders
 Remove-Item -LiteralPath "temp-metabase" -Force -Recurse
 Remove-Item -LiteralPath "temp-postgresql" -Force -Recurse
