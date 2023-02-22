e4cfb12e8e3d3a45df9cc148f3ab8a85605aab3c
Peter Platten <peter.platten@gov.bc.ca>
2023-02-22 09:36:38 -0800

    JanFeb 2023 PSR Updates

---
+++ send-usage-emails/open_postgres_port_dev.ps1
@@ -0,0 +1,13 @@
+
+# Script requires that user is already logged in to OC with appropriate credentials
+# Correct project must be selected
+
+# Clean up any open port binds from previous runs
+taskkill /im oc.exe /f
+
+# Get the data pod name from the current namespace
+$POSTGRES_DB_POD=oc get pods --selector name=postgresql --field-selector status.phase=Running -o custom-columns=POD:.metadata.name --no-headers
+# Launch a new thread with a port bind
+$PATH=$Env:Path
+# Start-Process "oc-port-forward" cmd /k "SET PATH=$CD;$PATH & oc port-forward $POSTGRES_DB_POD 5432:5432"
+Start-Process -FilePath "oc.exe" -ArgumentList "port-forward",$POSTGRES_DB_POD,"5432:5432" -PassThru
